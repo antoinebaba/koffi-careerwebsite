e6166ed2fee954c732b35b2752dda0e345869d75
Koffi Agbewonou Antoine BABA <antoinebaba5@gmail.com>
2023-02-25 17:12:19 +0300

    Creating hml and static folder

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello Koffi!"
+    return render_template('home.html')
 
 if __name__ == '__name__':
     app.run(host='0.0.0.0', debug=True)
