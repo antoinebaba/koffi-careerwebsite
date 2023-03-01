8e947750f16517d43d2f0d5cbd1347ad03b09535
Koffi Agbewonou Antoine BABA <antoinebaba5@gmail.com>
2023-03-01 17:54:53 +0300

    Added responsive image

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 app = Flask(__name__)
 
 JOBS=[
@@ -36,5 +36,9 @@ def hello_Koffi():
     return render_template('home.html', jobs=JOBS,
                            company_name='Koffi')
 
+#Return json
+@app.route("/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 if __name__ == '__name__':
     app.run(host='0.0.0.0', debug=True)
